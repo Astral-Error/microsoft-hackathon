6d99125ffa97f4a8933f7098c5218dd3f013be29
Astral-Error <182073205+Astral-Error@users.noreply.github.com>
2026-08-04 16:56:12 +0530

    Repo nuke

--- src/main/java/com/microsoft/libmanagement/dto/BorrowRequestCreateRequest.java
+++
@@ -1,6 +0,0 @@
-package com.microsoft.libmanagement.dto;
-
-import java.time.LocalDate;
-
-public record BorrowRequestCreateRequest(Long memberId, String bookTitle, LocalDate startDate, LocalDate expectedReturnDate) {
-}
--- src/main/java/com/microsoft/libmanagement/dto/BorrowRequestResponse.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.libmanagement.dto;
-
-import com.microsoft.libmanagement.model.RequestStatus;
-
-import java.time.LocalDate;
-
-public record BorrowRequestResponse(Long id, Long memberId, String bookTitle, LocalDate startDate, LocalDate expectedReturnDate,
-                                   RequestStatus status, Long assignedCopyId, String reviewComment) {
-}
