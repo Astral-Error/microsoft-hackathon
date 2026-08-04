aa35774aa606a1cb11c27ff49e862a668375ae00
Astral-Error <182073205+Astral-Error@users.noreply.github.com>
2026-08-04 16:29:32 +0530

    Initial structure

---
+++ src/test/java/com/microsoft/libmanagement/LibmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.libmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
